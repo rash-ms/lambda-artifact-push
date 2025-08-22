b0b702cb64c1d1e5129ba70c0aaa2f2c1fabb30c
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-08-22 13:04:53 +0100

    commit sqs to lambda

--- infra/terraform/cppv2-sqs-lambda-mapping/main_cppv2_lambda_eu.tf
+++ infra/terraform/cppv2-sqs-lambda-mapping/main_cppv2_lambda_eu.tf
@@ -1,144 +1,144 @@
-# # 1) Look up the source object (US)
-data "aws_s3_bucket_object" "src_zip_eu" {
-  provider = aws.us
-  bucket   = var.lambda_s3_bucket
-  key      = "${var.s3_key}/${var.handler_zip}.zip"
-}
-
-
-resource "null_resource" "zip_change_detector_eu" {
-  triggers = {
-    # Multiple triggers to detect changes
-    source_etag = try(data.aws_s3_bucket_object.src_zip_eu.etag)
-    # source_etag = try(data.aws_s3_bucket_object.src_zip_eu.etag, timestamp())
-    source_key = "${var.s3_key}/${var.handler_zip}.zip"
-    handler    = var.handler_zip
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-
-resource "aws_s3_object_copy" "zip_eu" {
-  provider = aws.eu
-  bucket   = "cn-infra-lambda-artifacts-stg-eu"
-  key      = "${var.s3_key}/${var.handler_zip}.zip"
-
-  source = "${var.lambda_s3_bucket}/${var.s3_key}/${var.handler_zip}.zip"
-
-  # Recopy when the source changes
-  lifecycle {
-    replace_triggered_by = [null_resource.zip_change_detector_eu]
-  }
-
-  depends_on = [data.aws_s3_bucket_object.src_zip_eu]
-}
-
-
-# resource "null_resource" "s3_copy_eu" {
+# # # 1) Look up the source object (US)
+# data "aws_s3_bucket_object" "src_zip_eu" {
+#   provider = aws.us
+#   bucket   = var.lambda_s3_bucket
+#   key      = "${var.s3_key}/${var.handler_zip}.zip"
+# }
+#
+#
+# resource "null_resource" "zip_change_detector_eu" {
 #   triggers = {
-#     source_etag = try(data.aws_s3_bucket_object.src_zip_eu.etag, timestamp())
-#     source_key  = "${var.s3_key}/${var.handler_zip}.zip"
+#     # Multiple triggers to detect changes
+#     source_etag = try(data.aws_s3_bucket_object.src_zip_eu.etag)
+#     # source_etag = try(data.aws_s3_bucket_object.src_zip_eu.etag, timestamp())
+#     source_key = "${var.s3_key}/${var.handler_zip}.zip"
+#     handler    = var.handler_zip
 #   }
 #
-#   provisioner "local-exec" {
-#     command = <<-EOT
-#       aws s3 cp \
-#         s3://cn-infra-lambda-artifacts/${var.s3_key}/${var.handler_zip}.zip \
-#         s3://cn-infra-lambda-artifacts-stg-eu/${var.s3_key}/${var.handler_zip}.zip
-#     EOT
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 #
-#     environment = {
-#       AWS_DEFAULT_REGION = "eu-central-1"
-#     }
+#
+# resource "aws_s3_object_copy" "zip_eu" {
+#   provider = aws.eu
+#   bucket   = "cn-infra-lambda-artifacts-stg-eu"
+#   key      = "${var.s3_key}/${var.handler_zip}.zip"
+#
+#   source = "${var.lambda_s3_bucket}/${var.s3_key}/${var.handler_zip}.zip"
+#
+#   # Recopy when the source changes
+#   lifecycle {
+#     replace_triggered_by = [null_resource.zip_change_detector_eu]
 #   }
 #
 #   depends_on = [data.aws_s3_bucket_object.src_zip_eu]
 # }
-
-
-data "aws_kinesis_firehose_delivery_stream" "userplatform_cpp_firehose_delivery_stream_eu" {
-  provider = aws.eu
-  name     = "userplatform_cpp_firehose_delivery_stream_eu"
-}
-
-# data "aws_kms_alias" "cppv2_kms_key_lambda_eu" {
+#
+#
+# # resource "null_resource" "s3_copy_eu" {
+# #   triggers = {
+# #     source_etag = try(data.aws_s3_bucket_object.src_zip_eu.etag, timestamp())
+# #     source_key  = "${var.s3_key}/${var.handler_zip}.zip"
+# #   }
+# #
+# #   provisioner "local-exec" {
+# #     command = <<-EOT
+# #       aws s3 cp \
+# #         s3://cn-infra-lambda-artifacts/${var.s3_key}/${var.handler_zip}.zip \
+# #         s3://cn-infra-lambda-artifacts-stg-eu/${var.s3_key}/${var.handler_zip}.zip
+# #     EOT
+# #
+# #     environment = {
+# #       AWS_DEFAULT_REGION = "eu-central-1"
+# #     }
+# #   }
+# #
+# #   depends_on = [data.aws_s3_bucket_object.src_zip_eu]
+# # }
+#
+#
+# data "aws_kinesis_firehose_delivery_stream" "userplatform_cpp_firehose_delivery_stream_eu" {
 #   provider = aws.eu
-#   name     = "alias/aws/lambda"
+#   name     = "userplatform_cpp_firehose_delivery_stream_eu"
 # }
-
-
-resource "aws_sqs_queue" "userplatform_cppv2_sqs_dlq_eu" {
-  provider                  = aws.eu
-  name                      = "userplatform_cppv2_sqs_dlq_eu"
-  message_retention_seconds = 1209600 # 14 days
-}
-
-resource "aws_sqs_queue" "userplatform_cppv2_sqs_eu" {
-  provider = aws.eu
-  name     = "userplatform_cppv2_sqs_eu"
-
-  visibility_timeout_seconds = 1080   #  6x >= lambda timeout
-  message_retention_seconds  = 604800 # 7 days
-  receive_wait_time_seconds  = 10     # polling period
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.userplatform_cppv2_sqs_dlq_eu.arn
-    maxReceiveCount     = 5
-  })
-}
-
-# ================= Lambda Function =================
-
-resource "aws_lambda_function" "cppv2_sqs_lambda_firehose_eu" {
-  provider         = aws.eu
-  function_name    = "cppv2_sqs_lambda_firehose_eu"
-  s3_bucket        = aws_s3_object_copy.zip_eu.bucket
-  s3_key           = aws_s3_object_copy.zip_eu.key
-  source_code_hash = aws_s3_object_copy.zip_eu.etag
-
-  # s3_bucket = "cn-infra-lambda-artifacts-stg-eu"
-  # s3_key    = "${var.s3_key}/${var.handler_zip}.zip"
-  # source_code_hash = null_resource.s3_copy_eu.triggers.source_etag
-
-  handler     = "${var.handler_zip}.send_to_firehose"
-  runtime     = "python3.9"
-  timeout     = 180
-  memory_size = 1024
-  role        = data.aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
-
-  kms_key_arn = null
-  # kms_key_arn = data.aws_kms_alias.cppv2_kms_key_lambda_eu.target_key_arn
-
-  environment {
-    variables = {
-      FIREHOSE_STREAM     = data.aws_kinesis_firehose_delivery_stream.userplatform_cpp_firehose_delivery_stream_eu.name
-      REGION              = local.route_configs["eu"].region
-      EVENTS_BUCKET       = local.route_configs["eu"].bucket
-      ERROR_EVENTS_PREFIX = "raw/cpp-v2-raw-errors/"
-    }
-  }
-  depends_on = [aws_s3_object_copy.zip_eu]
-  # depends_on = [null_resource.s3_copy_eu]
-}
-
-resource "aws_cloudwatch_log_group" "cpv2_sqs_lambda_firehose_log_eu" {
-  provider          = aws.eu
-  name              = "/aws/lambda/${aws_lambda_function.cppv2_sqs_lambda_firehose_eu.function_name}"
-  retention_in_days = 14
-}
-
-
-resource "aws_lambda_event_source_mapping" "cpp_sqs_lambda_trigger_eu" {
-  provider                           = aws.eu
-  event_source_arn                   = aws_sqs_queue.userplatform_cppv2_sqs_eu.arn
-  function_name                      = aws_lambda_function.cppv2_sqs_lambda_firehose_eu.arn
-  batch_size                         = 10
-  maximum_batching_window_in_seconds = 5
-  function_response_types            = ["ReportBatchItemFailures"]
-  enabled                            = true
-
-  depends_on = [aws_cloudwatch_log_group.cpv2_sqs_lambda_firehose_log_eu]
-}
+#
+# # data "aws_kms_alias" "cppv2_kms_key_lambda_eu" {
+# #   provider = aws.eu
+# #   name     = "alias/aws/lambda"
+# # }
+#
+#
+# resource "aws_sqs_queue" "userplatform_cppv2_sqs_dlq_eu" {
+#   provider                  = aws.eu
+#   name                      = "userplatform_cppv2_sqs_dlq_eu"
+#   message_retention_seconds = 1209600 # 14 days
+# }
+#
+# resource "aws_sqs_queue" "userplatform_cppv2_sqs_eu" {
+#   provider = aws.eu
+#   name     = "userplatform_cppv2_sqs_eu"
+#
+#   visibility_timeout_seconds = 1080   #  6x >= lambda timeout
+#   message_retention_seconds  = 604800 # 7 days
+#   receive_wait_time_seconds  = 10     # polling period
+#
+#   redrive_policy = jsonencode({
+#     deadLetterTargetArn = aws_sqs_queue.userplatform_cppv2_sqs_dlq_eu.arn
+#     maxReceiveCount     = 5
+#   })
+# }
+#
+# # ================= Lambda Function =================
+#
+# resource "aws_lambda_function" "cppv2_sqs_lambda_firehose_eu" {
+#   provider         = aws.eu
+#   function_name    = "cppv2_sqs_lambda_firehose_eu"
+#   s3_bucket        = aws_s3_object_copy.zip_eu.bucket
+#   s3_key           = aws_s3_object_copy.zip_eu.key
+#   source_code_hash = aws_s3_object_copy.zip_eu.etag
+#
+#   # s3_bucket = "cn-infra-lambda-artifacts-stg-eu"
+#   # s3_key    = "${var.s3_key}/${var.handler_zip}.zip"
+#   # source_code_hash = null_resource.s3_copy_eu.triggers.source_etag
+#
+#   handler     = "${var.handler_zip}.send_to_firehose"
+#   runtime     = "python3.9"
+#   timeout     = 180
+#   memory_size = 1024
+#   role        = data.aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
+#
+#   kms_key_arn = null
+#   # kms_key_arn = data.aws_kms_alias.cppv2_kms_key_lambda_eu.target_key_arn
+#
+#   environment {
+#     variables = {
+#       FIREHOSE_STREAM     = data.aws_kinesis_firehose_delivery_stream.userplatform_cpp_firehose_delivery_stream_eu.name
+#       REGION              = local.route_configs["eu"].region
+#       EVENTS_BUCKET       = local.route_configs["eu"].bucket
+#       ERROR_EVENTS_PREFIX = "raw/cpp-v2-raw-errors/"
+#     }
+#   }
+#   depends_on = [aws_s3_object_copy.zip_eu]
+#   # depends_on = [null_resource.s3_copy_eu]
+# }
+#
+# resource "aws_cloudwatch_log_group" "cpv2_sqs_lambda_firehose_log_eu" {
+#   provider          = aws.eu
+#   name              = "/aws/lambda/${aws_lambda_function.cppv2_sqs_lambda_firehose_eu.function_name}"
+#   retention_in_days = 14
+# }
+#
+#
+# resource "aws_lambda_event_source_mapping" "cpp_sqs_lambda_trigger_eu" {
+#   provider                           = aws.eu
+#   event_source_arn                   = aws_sqs_queue.userplatform_cppv2_sqs_eu.arn
+#   function_name                      = aws_lambda_function.cppv2_sqs_lambda_firehose_eu.arn
+#   batch_size                         = 10
+#   maximum_batching_window_in_seconds = 5
+#   function_response_types            = ["ReportBatchItemFailures"]
+#   enabled                            = true
+#
+#   depends_on = [aws_cloudwatch_log_group.cpv2_sqs_lambda_firehose_log_eu]
+# }
--- infra/terraform/cppv2-sqs-lambda-mapping/main_cppv2_lambda_us.tf
+++ infra/terraform/cppv2-sqs-lambda-mapping/main_cppv2_lambda_us.tf
@@ -1,153 +1,153 @@
-data "aws_iam_role" "cpp_integration_apigw_evtbridge_firehose_logs_role" {
-  name = "cpp_integration_apigw_evtbridge_firehose_logs_role"
-}
-
-data "aws_kinesis_firehose_delivery_stream" "userplatform_cpp_firehose_delivery_stream_us" {
-  provider = aws.us
-  name     = "userplatform_cpp_firehose_delivery_stream_us"
-}
-
-# data "aws_kms_alias" "cppv2_kms_key_lambda_us" {
-#   provider = aws.us
-#   name     = "alias/aws/lambda"
+# data "aws_iam_role" "cpp_integration_apigw_evtbridge_firehose_logs_role" {
+#   name = "cpp_integration_apigw_evtbridge_firehose_logs_role"
 # }
-
-resource "aws_iam_role_policy" "cppv2_lambda_sqs_permissions" {
-  name = "cppv2_lambda_sqs_permissions"
-  role = data.aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      # {
-      #   Effect = "Allow"
-      #   Action = [
-      #     "sqs:ReceiveMessage",
-      #     "sqs:DeleteMessage",
-      #     "sqs:GetQueueAttributes",
-      #     "sqs:ChangeMessageVisibility",
-      #     "sqs:GetQueueUrl",
-      #     "sqs:ListDeadLetterSourceQueues",
-      #     "sqs:SendMessageBatch",
-      #     "sqs:PurgeQueue",
-      #     "sqs:SendMessage",
-      #     "sqs:CreateQueue",
-      #     "sqs:ListQueueTags",
-      #     "sqs:ChangeMessageVisibilityBatch",
-      #     "sqs:SetQueueAttributes"
-      #   ]
-      #   Resource = [
-      #     aws_sqs_queue.userplatform_cppv2_sqs_us.arn,
-      #     aws_sqs_queue.userplatform_cppv2_sqs_dlq_us.arn,
-      #     aws_sqs_queue.userplatform_cppv2_sqs_eu.arn,
-      #     aws_sqs_queue.userplatform_cppv2_sqs_dlq_eu.arn,
-      #     aws_sqs_queue.userplatform_cppv2_sqs_ap.arn,
-      #     aws_sqs_queue.userplatform_cppv2_sqs_dlq_ap.arn
-      #   ]
-      # },
-      # {
-      #   "Effect" : "Allow",
-      #   Action : [
-      #     "sqs:GetQueueUrl",
-      #     "sqs:ListQueues"
-      #   ],
-      #   Resource = [
-      #     aws_sqs_queue.userplatform_cppv2_sqs_us.arn,
-      #     aws_sqs_queue.userplatform_cppv2_sqs_dlq_us.arn,
-      #     aws_sqs_queue.userplatform_cppv2_sqs_eu.arn,
-      #     aws_sqs_queue.userplatform_cppv2_sqs_dlq_eu.arn,
-      #     aws_sqs_queue.userplatform_cppv2_sqs_ap.arn,
-      #     aws_sqs_queue.userplatform_cppv2_sqs_dlq_ap.arn
-      #   ]
-      # },
-      {
-        "Effect" : "Allow",
-        Action : [
-          "kms:Decrypt",
-          "kms:DescribeKey",
-          "kms:Encrypt",
-          "kms:ReEncrypt*",
-          "kms:GenerateDataKey*",
-          "kms:DescribeKey",
-          "kms:GenerateDataKeyWithoutPlaintext"
-        ],
-
-        Resource = [
-          "arn:aws:kms:${local.route_configs["us"].region}:${var.account_id}:alias/aws/lambda",
-          "arn:aws:kms:${local.route_configs["eu"].region}:${var.account_id}:alias/aws/lambda",
-          "arn:aws:kms:${local.route_configs["ap"].region}:${var.account_id}:alias/aws/lambda"
-        ]
-        # Resource = [
-        #   data.aws_kms_alias.cppv2_kms_key_lambda_us.target_key_arn,
-        #   data.aws_kms_alias.cppv2_kms_key_lambda_eu.target_key_arn,
-        #   data.aws_kms_alias.cppv2_kms_key_lambda_ap.target_key_arn
-        # ]
-      }
-    ]
-  })
-}
-
-resource "aws_sqs_queue" "userplatform_cppv2_sqs_dlq_us" {
-  provider                  = aws.us
-  name                      = "userplatform_cppv2_sqs_dlq_us"
-  message_retention_seconds = 1209600 # 14 days
-}
-
-resource "aws_sqs_queue" "userplatform_cppv2_sqs_us" {
-  provider = aws.us
-  name     = "userplatform_cppv2_sqs_us"
-
-  visibility_timeout_seconds = 1080   #  6x >= lambda timeout
-  message_retention_seconds  = 604800 # 7 days
-  receive_wait_time_seconds  = 10     # polling period
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.userplatform_cppv2_sqs_dlq_us.arn
-    maxReceiveCount     = 5
-  })
-}
-
-# ================= Lambda Function =================
-
-resource "aws_lambda_function" "cpv2_sqs_lambda_firehose_us" {
-  provider      = aws.us
-  function_name = "cppv2_sqs_lambda_firehose_us"
-  s3_bucket     = var.lambda_s3_bucket
-  s3_key        = "${var.s3_key}/${var.handler_zip}.zip"
-  handler       = "${var.handler_zip}.send_to_firehose"
-  runtime       = "python3.9"
-  timeout       = 180
-  memory_size   = 1024
-  role          = data.aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
-
-  kms_key_arn = null
-  # kms_key_arn = data.aws_kms_alias.cppv2_kms_key_lambda_us.target_key_arn
-
-  environment {
-    variables = {
-      FIREHOSE_STREAM     = data.aws_kinesis_firehose_delivery_stream.userplatform_cpp_firehose_delivery_stream_us.name
-      REGION              = local.route_configs["us"].region
-      EVENTS_BUCKET       = local.route_configs["us"].bucket
-      ERROR_EVENTS_PREFIX = "raw/cpp-v2-raw-errors/"
-    }
-  }
-}
-
-resource "aws_cloudwatch_log_group" "cpv2_sqs_lambda_firehose_log_us" {
-  provider          = aws.us # ← add this so it’s in us-east-1
-  name              = "/aws/lambda/${aws_lambda_function.cpv2_sqs_lambda_firehose_us.function_name}"
-  retention_in_days = 14
-}
-
-
-resource "aws_lambda_event_source_mapping" "cpp_sqs_lambda_trigger_us" {
-  provider                           = aws.us
-  event_source_arn                   = aws_sqs_queue.userplatform_cppv2_sqs_us.arn
-  function_name                      = aws_lambda_function.cpv2_sqs_lambda_firehose_us.arn
-  batch_size                         = 10
-  maximum_batching_window_in_seconds = 5
-  function_response_types            = ["ReportBatchItemFailures"]
-  enabled                            = true
-
-  depends_on = [aws_cloudwatch_log_group.cpv2_sqs_lambda_firehose_log_us]
-}
+#
+# data "aws_kinesis_firehose_delivery_stream" "userplatform_cpp_firehose_delivery_stream_us" {
+#   provider = aws.us
+#   name     = "userplatform_cpp_firehose_delivery_stream_us"
+# }
+#
+# # data "aws_kms_alias" "cppv2_kms_key_lambda_us" {
+# #   provider = aws.us
+# #   name     = "alias/aws/lambda"
+# # }
+#
+# resource "aws_iam_role_policy" "cppv2_lambda_sqs_permissions" {
+#   name = "cppv2_lambda_sqs_permissions"
+#   role = data.aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.name
+#
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       # {
+#       #   Effect = "Allow"
+#       #   Action = [
+#       #     "sqs:ReceiveMessage",
+#       #     "sqs:DeleteMessage",
+#       #     "sqs:GetQueueAttributes",
+#       #     "sqs:ChangeMessageVisibility",
+#       #     "sqs:GetQueueUrl",
+#       #     "sqs:ListDeadLetterSourceQueues",
+#       #     "sqs:SendMessageBatch",
+#       #     "sqs:PurgeQueue",
+#       #     "sqs:SendMessage",
+#       #     "sqs:CreateQueue",
+#       #     "sqs:ListQueueTags",
+#       #     "sqs:ChangeMessageVisibilityBatch",
+#       #     "sqs:SetQueueAttributes"
+#       #   ]
+#       #   Resource = [
+#       #     aws_sqs_queue.userplatform_cppv2_sqs_us.arn,
+#       #     aws_sqs_queue.userplatform_cppv2_sqs_dlq_us.arn,
+#       #     aws_sqs_queue.userplatform_cppv2_sqs_eu.arn,
+#       #     aws_sqs_queue.userplatform_cppv2_sqs_dlq_eu.arn,
+#       #     aws_sqs_queue.userplatform_cppv2_sqs_ap.arn,
+#       #     aws_sqs_queue.userplatform_cppv2_sqs_dlq_ap.arn
+#       #   ]
+#       # },
+#       # {
+#       #   "Effect" : "Allow",
+#       #   Action : [
+#       #     "sqs:GetQueueUrl",
+#       #     "sqs:ListQueues"
+#       #   ],
+#       #   Resource = [
+#       #     aws_sqs_queue.userplatform_cppv2_sqs_us.arn,
+#       #     aws_sqs_queue.userplatform_cppv2_sqs_dlq_us.arn,
+#       #     aws_sqs_queue.userplatform_cppv2_sqs_eu.arn,
+#       #     aws_sqs_queue.userplatform_cppv2_sqs_dlq_eu.arn,
+#       #     aws_sqs_queue.userplatform_cppv2_sqs_ap.arn,
+#       #     aws_sqs_queue.userplatform_cppv2_sqs_dlq_ap.arn
+#       #   ]
+#       # },
+#       # {
+#       #   "Effect" : "Allow",
+#       #   Action : [
+#       #     "kms:Decrypt",
+#       #     "kms:DescribeKey",
+#       #     "kms:Encrypt",
+#       #     "kms:ReEncrypt*",
+#       #     "kms:GenerateDataKey*",
+#       #     "kms:DescribeKey",
+#       #     "kms:GenerateDataKeyWithoutPlaintext"
+#       #   ],
+#       #
+#       #   Resource = [
+#       #     "arn:aws:kms:${local.route_configs["us"].region}:${var.account_id}:alias/aws/lambda",
+#       #     "arn:aws:kms:${local.route_configs["eu"].region}:${var.account_id}:alias/aws/lambda",
+#       #     "arn:aws:kms:${local.route_configs["ap"].region}:${var.account_id}:alias/aws/lambda"
+#       #   ]
+#       #   # Resource = [
+#       #   #   data.aws_kms_alias.cppv2_kms_key_lambda_us.target_key_arn,
+#       #   #   data.aws_kms_alias.cppv2_kms_key_lambda_eu.target_key_arn,
+#       #   #   data.aws_kms_alias.cppv2_kms_key_lambda_ap.target_key_arn
+#       #   # ]
+#       # }
+#     ]
+#   })
+# }
+#
+# resource "aws_sqs_queue" "userplatform_cppv2_sqs_dlq_us" {
+#   provider                  = aws.us
+#   name                      = "userplatform_cppv2_sqs_dlq_us"
+#   message_retention_seconds = 1209600 # 14 days
+# }
+#
+# resource "aws_sqs_queue" "userplatform_cppv2_sqs_us" {
+#   provider = aws.us
+#   name     = "userplatform_cppv2_sqs_us"
+#
+#   visibility_timeout_seconds = 1080   #  6x >= lambda timeout
+#   message_retention_seconds  = 604800 # 7 days
+#   receive_wait_time_seconds  = 10     # polling period
+#
+#   redrive_policy = jsonencode({
+#     deadLetterTargetArn = aws_sqs_queue.userplatform_cppv2_sqs_dlq_us.arn
+#     maxReceiveCount     = 5
+#   })
+# }
+#
+# # ================= Lambda Function =================
+#
+# resource "aws_lambda_function" "cpv2_sqs_lambda_firehose_us" {
+#   provider      = aws.us
+#   function_name = "cppv2_sqs_lambda_firehose_us"
+#   s3_bucket     = var.lambda_s3_bucket
+#   s3_key        = "${var.s3_key}/${var.handler_zip}.zip"
+#   handler       = "${var.handler_zip}.send_to_firehose"
+#   runtime       = "python3.9"
+#   timeout       = 180
+#   memory_size   = 1024
+#   role          = data.aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
+#
+#   kms_key_arn = null
+#   # kms_key_arn = data.aws_kms_alias.cppv2_kms_key_lambda_us.target_key_arn
+#
+#   environment {
+#     variables = {
+#       FIREHOSE_STREAM     = data.aws_kinesis_firehose_delivery_stream.userplatform_cpp_firehose_delivery_stream_us.name
+#       REGION              = local.route_configs["us"].region
+#       EVENTS_BUCKET       = local.route_configs["us"].bucket
+#       ERROR_EVENTS_PREFIX = "raw/cpp-v2-raw-errors/"
+#     }
+#   }
+# }
+#
+# resource "aws_cloudwatch_log_group" "cpv2_sqs_lambda_firehose_log_us" {
+#   provider          = aws.us # ← add this so it’s in us-east-1
+#   name              = "/aws/lambda/${aws_lambda_function.cpv2_sqs_lambda_firehose_us.function_name}"
+#   retention_in_days = 14
+# }
+#
+#
+# resource "aws_lambda_event_source_mapping" "cpp_sqs_lambda_trigger_us" {
+#   provider                           = aws.us
+#   event_source_arn                   = aws_sqs_queue.userplatform_cppv2_sqs_us.arn
+#   function_name                      = aws_lambda_function.cpv2_sqs_lambda_firehose_us.arn
+#   batch_size                         = 10
+#   maximum_batching_window_in_seconds = 5
+#   function_response_types            = ["ReportBatchItemFailures"]
+#   enabled                            = true
+#
+#   depends_on = [aws_cloudwatch_log_group.cpv2_sqs_lambda_firehose_log_us]
+# }
